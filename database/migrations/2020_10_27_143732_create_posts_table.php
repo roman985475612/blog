15409c3dd5ee528b3416116ab864f2bf34e1beff
Roman <roman985475612@gmail.com>
2020-10-27 18:20:36 +0300

    Chapter 5 complete

--- database/migrations/2020_10_27_143732_create_posts_table.php
+++ database/migrations/2020_10_27_143732_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('is_admin')->default(0);
+            $table->string('title');
+            $table->string('slug');
+            $table->text('content');
             $table->integer('status')->default(0);
-            $table->rememberToken();
+            $table->integer('views')->default(0);
+            $table->integer('category_id')->nullable();
+            $table->integer('user_id')->nullable();
+            $table->integer('is_featured')->default(0);
             $table->timestamps();
         });
     }
@@ -33,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 }
